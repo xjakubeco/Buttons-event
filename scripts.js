ac5dee8d5e67e1e6ba2063bfafd86c09d6be3adf
xjakubeco <102169575+xjakubeco@users.noreply.github.com>
2022-03-24 23:29:01 +0100

    added index.html and others

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$("#Tlacidlo1").click((event) =>{
+    console.log('Yeah, you clicked me');
+})
+
+$("#Tlacidlo2").click((event) =>{
+    $("#Tlacidlo1").text("ssss");
+})
+
+$("#Tlacidlo3").click((event) =>{
+    $("button").css("background-color", "green");
+})
